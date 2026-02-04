89071a6aead65165b539343287f7978af7bb3266
Mimonata <sabina_pitul@yahoo.de>
2026-02-04 07:18:18 +0100

    r

--- py_web_client/webclient.py
+++ py_web_client/webclient.py
@@ -6,16 +6,38 @@
 #    By: spitul <spitul@student.42berlin.de>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/01/30 07:38:54 by spitul            #+#    #+#              #
-#    Updated: 2026/02/01 20:27:36 by spitul           ###   ########.fr        #
+#    Updated: 2026/02/04 07:18:13 by spitul           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import socket
 import sys
+from urllib import urlparse
 
 # python webclient.py example.com
 
-def	webclient(sys.argv) -> str
+def	receive_response(sock):
+	
+	response = b""
+	while True:
+		data = sock.recv(4096)
+		if not data:
+			break
+		response += data
+	return response.decode()
+
+def	send_http_request(url, port) -> str:
+
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.connect(url.hostname, port)
+	request = f"GET {url.path} HTTP/1.1\r\nHost: {url.hostname}\r\nConnection: close\r\n\r\n"
+	sock.sendall(request.encode())
+	response = receive_response(sock)
+	sock.close()
+	return response
+
+def	webclient(sys.argv) -> str:
+
 	input = sys.argv.split(" ")
 	if (len(input) == 3)
 		port = atoi(input[2])
@@ -27,6 +49,6 @@ def	webclient(sys.argv) -> str
 	
 	if "://" not in url
 		url = "http://" + url
-	parts = url.split("://")[1].split("/")
-	
-	
+	parsed = urlparse(url)
+	response = send_http_request(parsed, port)
+	print(response)
