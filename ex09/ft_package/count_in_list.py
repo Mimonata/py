bd5f26ec888482253c2919c9f1ff048b278380df
Mimonata <sabina_pitul@yahoo.de>
2026-06-16 21:33:37 +0200

    ft_package done

--- ex09/ft_package/count_in_list.py
+++ ex09/ft_package/count_in_list.py
@@ -1,13 +1,18 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    __init__.py                                        :+:      :+:    :+:    #
+#    count_in_list.py                                   :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: spitul <spitul@student.42berlin.de>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/06/16 21:25:32 by spitul            #+#    #+#              #
-#    Updated: 2026/06/16 21:26:20 by spitul           ###   ########.fr        #
+#    Created: 2026/06/16 20:59:42 by spitul            #+#    #+#              #
+#    Updated: 2026/06/16 21:02:24 by spitul           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-from ft_package.count_in_list import count_in_list
+def count_in_list(lst: list, var: str) -> int:
+	counter = 0
+	for x in lst:
+		if x == var:
+			counter += 1
+	return counter
