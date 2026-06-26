61edb84296e0ab1186dc746ea87c7608f5874d3a
Mimonata <sabina_pitul@yahoo.de>
2026-06-26 22:13:34 +0200

    ex04

--- mod1/ex04/rotate.py
+++ mod1/ex04/rotate.py
@@ -1,12 +1,12 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    zoom.py                                            :+:      :+:    :+:    #
+#    rotate.py                                          :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: spitul <spitul@student.42berlin.de>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/06/22 22:05:09 by spitul            #+#    #+#              #
-#    Updated: 2026/06/26 22:12:38 by spitul           ###   ########.fr        #
+#    Created: 2026/06/25 20:27:44 by spitul            #+#    #+#              #
+#    Updated: 2026/06/26 22:10:15 by spitul           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,16 +16,21 @@ import numpy
 
 sys.path.append("../ex02")
 from load_image import ft_load
+sys.path.append("../ex03")
+from zoom import zoom
 
-def zoom():
-	arr = ft_load("test_img.jpg")
-	zoomed = arr[200:600, 200:600, 0]
-	plt.imshow(zoomed, cmap="gray")
+def	rotate():
+	# arr = ft_load("test_img.jpg")
+	zoomed = zoom()
+	rows, cols = zoomed.shape
+	transposed = numpy.zeros((cols, rows), dtype = zoomed.dtype)
+	for y in range(rows):
+		for x in range(cols):
+			transposed[x][y] = zoomed[y][x]
+	plt.imshow(transposed, cmap="gray")
 	plt.savefig("output.jpg")
-	print(f"New shape after slicing {zoomed.shape}")
-	print(zoomed)
-	return zoomed
+	print(f"New shape after transpose: {transposed.shape}")
+	print(transposed)
 	
 if __name__ == "__main__":
-	zoom()
-	
+	rotate()
