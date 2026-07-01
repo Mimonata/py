09b616389ad1de643d111c953f5c1f5262c7dcfb
Mimonata <sabina_pitul@yahoo.de>
2026-07-01 21:36:10 +0200

    ft_inverse

--- mod1/ex05/pimp_image.py
+++ mod1/ex05/pimp_image.py
@@ -6,21 +6,35 @@
 #    By: spitul <spitul@student.42berlin.de>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/06/29 07:33:09 by spitul            #+#    #+#              #
-#    Updated: 2026/06/29 07:38:04 by spitul           ###   ########.fr        #
+#    Updated: 2026/07/01 21:35:16 by spitul           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-def ft_invert(array) -> array:
+import matplotlib.pyplot as plt
+import numpy as np
+# # np.uint8(255) + 1
+# a = np.array([100,200,30])
+# b = 255 - a
+# print(a)
+# print(b)
+
+def ft_invert(array) -> np.ndarray:
+	"""This function inverts the colors in an image"""
+	inverted = 255 - array
+	plt.imshow(inverted)
+	plt.show()
+	print(inverted)
+	return inverted
 
 
-def ft_red(array) -> array:
+# def ft_red(array) -> array:
 
 
-def ft_green(array) -> array:
+# def ft_green(array) -> array:
 
 
-def ft_blue(array) -> array:
+# def ft_blue(array) -> array:
 
 
-def ft_grey(array) -> array:
+# def ft_grey(array) -> array:
 
