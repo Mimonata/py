580aa524ea25cf3327f0c398ac83ee54b187585a
Mimonata <sabina_pitul@yahoo.de>
2026-07-02 21:34:31 +0200

    ft red

--- mod1/ex05/pimp_image.py
+++ mod1/ex05/pimp_image.py
@@ -6,7 +6,7 @@
 #    By: spitul <spitul@student.42berlin.de>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/06/29 07:33:09 by spitul            #+#    #+#              #
-#    Updated: 2026/07/02 07:50:41 by spitul           ###   ########.fr        #
+#    Updated: 2026/07/02 21:33:05 by spitul           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -39,15 +39,42 @@ def ft_red(array) -> np.ndarray:
 	print(reddend)
 	return reddend
 
-# def ft_green(array) -> array:
+def ft_green(array) -> np.ndarray:
+	"""Function ft_green renders a picture in its green tones"""
+	greened = array.copy()
+	greened[:, :, 0] = array[:, :, 0] - array[:, :, 0]
+	greened[:, :, 2] = array[:, :, 2] - array[:, :, 2]
+	plt.imshow(greened)
+	plt.show()
+	print(greened)
+	return greened
 
 
-# def ft_blue(array) -> array:
+def ft_blue(array) -> np.ndarray:
+	"""Function ft_blue for displaying the blue tones of an image"""
+	blued = array.copy()
+	blued[:, :, 0] = 0
+	blued[:, :, 1] = 0
+	plt.imshow(blued)
+	plt.show()
+	print(blued)
+	return blued
 
 
-# def ft_grey(array) -> array:
+def ft_grey(array) -> np.ndarray:
+	"""ft_gray for black and white image display"""
+	greyed = array.copy()
+	temp = greyed.mean(axis=2, keepdims=True)
+	greyed[:] = temp
+	plt.imshow(greyed)
+	plt.show()
+	print(greyed)
+	return greyed
 
 if __name__ == "__main__":
 	array = ft_load("test_img.jpg")
 	ft_invert(array)
 	ft_red(array)
+	ft_green(array)
+	ft_blue(array)
+	ft_grey(array)
