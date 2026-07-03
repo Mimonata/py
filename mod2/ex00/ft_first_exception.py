850e1646c3e66ce171e432c47f421a8b9890ba4d
Mimonata <sabina_pitul@yahoo.de>
2026-07-03 22:44:23 +0200

    ex0

--- mod2/ex00/ft_first_exception.py
+++ mod2/ex00/ft_first_exception.py
@@ -6,7 +6,7 @@
 #    By: spitul <spitul@student.42berlin.de>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/07/03 07:11:57 by spitul            #+#    #+#              #
-#    Updated: 2026/07/03 07:19:22 by spitul           ###   ########.fr        #
+#    Updated: 2026/07/03 22:43:52 by spitul           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,5 +17,28 @@ import traceback
 # except ValueError as e:
 # 	traceback.print_tb(e.__traceback__)
 # 	print(type(e))
-a = [1,2,3]
-print(a[10])
+# a = [1,2,3]
+# print(a[10])
+
+def input_temperature(data: str) -> int:
+	rez = int(data)
+	print(f"Temperature is now {data}°\n")
+	return rez
+
+def test_temperature() -> None:
+	try:
+		print("Input data is '25'")
+		input_temperature("25")
+	except Exception as e:
+		print(f"Caught input_temperature error: {e}")
+	try:
+		print("Input data is 'abc'")
+		input_temperature("abc")
+	except Exception as e:
+		print(f"Caught input_temperature error: {e}")
+	
+if __name__ == "__main__":
+	print("\n=== Garden Temperature ===\n")
+	test_temperature()
+		
+		
