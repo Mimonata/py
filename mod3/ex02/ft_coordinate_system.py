48ceb7c7d4be7fac6b617ef77658af9afd561952
Mimonata <sabina_pitul@yahoo.de>
2026-07-08 22:34:47 +0200

    ex02

--- mod3/ex02/ft_coordinate_system.py
+++ mod3/ex02/ft_coordinate_system.py
@@ -6,7 +6,7 @@
 #    By: spitul <spitul@student.42berlin.de>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/07/07 20:18:46 by spitul            #+#    #+#              #
-#    Updated: 2026/07/08 22:25:45 by spitul           ###   ########.fr        #
+#    Updated: 2026/07/08 22:34:17 by spitul           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,21 +17,23 @@ def get_player_pos():
 		str = input("Enter new coordinates as floats in format 'x,y,z': ")
 		coord = str.split(",")
 		if (len(coord) != 3):
-			print("Invalif syntax")
+			print("Invalid syntax")
 			continue
 		try:
 			x = float(coord[0])
 			y = float(coord[1])
 			z = float(coord[2])
+			return (x, y, z)
 		except ValueError as e:
 			print("Error on parameter:", e)
-		return (x, y, z)
+		
 
 def main():
 	print("=== Game Coordinate System ===")
 	print("\nGet a first set of coordinates")
+	
 	coord1 = get_player_pos()
-	print(f"Got a first tuple({coord1[0]}, {coord1[1]}, {coord1[2]}")
+	print(f"Got a first tuple({coord1[0]}, {coord1[1]}, {coord1[2]})")
 	print(f"It includes X = {coord1[0]}, Y = {coord1[1]}, Z = {coord1[2]}")
 	distance = math.sqrt(coord1[0]**2 + coord1[1]**2 + coord1[2]**2)
 	print("Distance to center: ", round(distance, 4))
