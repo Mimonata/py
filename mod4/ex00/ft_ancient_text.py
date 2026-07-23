d8fcb69edcae49095f88c68621619b5c13ff2807
Mimonata <sabina_pitul@yahoo.de>
2026-07-23 20:14:42 +0200

    ex00

--- mod4/ex00/ft_ancient_text.py
+++ mod4/ex00/ft_ancient_text.py
@@ -6,12 +6,11 @@
 #    By: spitul <spitul@student.42berlin.de>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/07/20 20:53:31 by spitul            #+#    #+#              #
-#    Updated: 2026/07/23 20:12:27 by spitul           ###   ########.fr        #
+#    Updated: 2026/07/23 20:13:57 by spitul           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
-import io
 
 def main():
 	if len(sys.argv) != 2:
@@ -27,7 +26,7 @@ def main():
 		file.close()
 		print(f"File '{sys.argv[1]}' closed.")
 	except Exception as e:
-		print(f"Error opening '{file}':", e)
+		print(f"Error opening '{sys.argv[1]}':", e)
 		
 if __name__ == "__main__":
 	main()
