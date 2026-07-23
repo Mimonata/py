d370e475993d796d9e44fcd05800ffd665f276fc
Mimonata <sabina_pitul@yahoo.de>
2026-07-23 20:13:11 +0200

    ex00

--- mod4/ex00/ft_ancient_text.py
+++ mod4/ex00/ft_ancient_text.py
@@ -6,7 +6,30 @@
 #    By: spitul <spitul@student.42berlin.de>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/07/20 20:53:31 by spitul            #+#    #+#              #
-#    Updated: 2026/07/20 20:53:32 by spitul           ###   ########.fr        #
+#    Updated: 2026/07/23 20:12:27 by spitul           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+import sys
+import io
+
+def main():
+	if len(sys.argv) != 2:
+		print("Usage: ft_ancient_text.py <file>")
+		exit(0)
+	try:
+		print("=== Cyber Archives Recovery ===")
+		print(f"Accessing file '{sys.argv[1]}'")
+		file = open(sys.argv[1])
+		print("---\n")
+		print(file.read())
+		print("\n---")
+		file.close()
+		print(f"File '{sys.argv[1]}' closed.")
+	except Exception as e:
+		print(f"Error opening '{file}':", e)
+		
+if __name__ == "__main__":
+	main()
+	exit(0)
+	
